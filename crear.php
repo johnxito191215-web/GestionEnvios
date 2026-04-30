8fb92642aa0040790fc3d081ec2392a27f8eed10
johnxito191215-web <johnxito191215@gmail.com>
2026-04-30 18:41:02 -0500

    cambio 3

---
+++ crear.php
@@ -0,0 +1,24 @@
+<?php include("conexion.php"); ?>
+
+<?php
+if ($_POST) {
+    $codigo = $_POST['codigo'];
+    $descripcion = $_POST['descripcion'];
+    $destino = $_POST['destino'];
+
+    $sql = "INSERT INTO envios (codigo, descripcion, destino)
+            VALUES ('$codigo', '$descripcion', '$destino')";
+
+    $conn->query($sql);
+    header("Location: index.php");
+}
+?>
+
+<h2>Nuevo Envío</h2>
+
+<form method="POST">
+    Código: <input type="text" name="codigo" required><br>
+    Descripción: <input type="text" name="descripcion" required><br>
+    Destino: <input type="text" name="destino" required><br>
+    <button type="submit">Guardar</button>
+</form>
